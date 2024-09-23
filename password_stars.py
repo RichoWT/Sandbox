f752e3c565fe6e95555eb096ec95904d0519a548
RichoWT <itsalmightyricho@gmail.com>
2024-09-23 15:11:38 +0800

    test1

--- password_stars.py
+++ password_stars.py
@@ -1,6 +1,6 @@
 PASSWORD_LENGTH = 10
 
-password = input("Enter password: ")
+password = input("Enter password: " )
 while len(password) < PASSWORD_LENGTH:
     password = input("Enter password: ")
 
